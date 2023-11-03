d8dd9653994e686dd26edc2c9956013c125292d5
Sadman26 <fareless.ranger@gmail.com>
2023-11-03 16:46:56 +0600

    Added these files

---
+++ script.js
@@ -0,0 +1,13 @@
+let x = document.querySelector('h1');
+let btn = document.querySelector('button');
+let fun = () => {
+    x.innerHTML = new Date().toLocaleTimeString();
+}
+setInterval(fun, 1000);
+let arr = [];
+btn.addEventListener('click',
+    () => {
+        arr.push(new Date().toLocaleTimeString());
+        console.log(arr);
+    }
+)
